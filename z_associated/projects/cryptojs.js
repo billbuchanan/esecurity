6fdfb9eefd6b0e3b3c9f8cbf9cafd2c21b5fa0e7
Bill Buchanan <w.buchanan@napier.ac.uk>
2019-01-15 22:50:35 +0000

    Node.js install

--- z_associated/projects/cryptojs.js
+++ z_associated/projects/cryptojs.js
@@ -16,6 +16,15 @@ console.log("\n--- Hashes");
 console.log("MD5: ",MD5(message).toString());
 console.log("SHA-256: ",SHA256(message).toString());
 console.log("SHA-3: ",SHA3(message).toString());
+ 
+//    To do:
+//    crypto-js/sha1
+//    crypto-js/sha256
+//    crypto-js/sha224
+//    crypto-js/sha512
+//    crypto-js/sha384
+//    crypto-js/ripemd160
+
 
 console.log("\n--- AES");
 var ciphertext = AES.encrypt(message, password);
@@ -26,5 +35,52 @@ var plaintext = bytes.toString(CryptoJS.enc.Utf8);
 console.log("Cipher: ",ciphertext.toString());
 console.log("Plaintext: ",plaintext);
 
+// To do:
+//   crypto-js/tripledes
+//   crypto-js/rc4
+//   crypto-js/rabbit
+//   crypto-js/evpkdf
+
+
 console.log("\n--- HMAC-SHA1");
 console.log("HMAC: ",CryptoJS.HmacSHA1(message, password).toString());
+
+//  To do:
+//    crypto-js/hmac-md5
+//    crypto-js/hmac-sha256
+//    crypto-js/hmac-sha224
+//    crypto-js/hmac-sha512
+//    crypto-js/hmac-sha384
+//    crypto-js/hmac-sha3
+//    crypto-js/hmac-ripemd160
+
+
+
+// Padding to do:
+//    crypto-js/pad-pkcs7
+//    crypto-js/pad-ansix923
+//    crypto-js/pad-iso10126
+//    crypto-js/pad-iso97971
+//    crypto-js/pad-zeropadding
+//    crypto-js/pad-nopadding
+
+
+// Formats to do:
+
+//    crypto-js/enc-latin1
+//    crypto-js/enc-utf8
+//    crypto-js/enc-hex
+//    crypto-js/enc-utf16
+//    crypto-js/enc-base64
+
+
+// Modes to do:
+
+//     crypto-js/mode-cfb
+//     crypto-js/mode-ctr
+//     crypto-js/mode-ctr-gladman
+//     crypto-js/mode-ofb
+//     crypto-js/mode-ecb
+
+
+
