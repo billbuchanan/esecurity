07e4650067629294cc29621997ab20d63f9d99c8
Bill Buchanan <w.buchanan@napier.ac.uk>
2019-02-17 14:19:35 +0000

    Update

--- unit05_key_exchange/src/d_01.py
+++ unit05_key_exchange/src/d_01.py
@@ -1,55 +1,18 @@
-import hashlib
-import sys
+from os import urandom
+from eccsnacks.curve25519 import scalarmult, scalarmult_base
 import binascii
-import Padding
-import random
 
-from Crypto.Cipher import AES
-from Crypto import Random
+a = urandom(32)
+a_pub = scalarmult_base(a)
 
-msg="test"
+b = urandom(32)
+b_pub = scalarmult_base(b)
 
-def encrypt(word,key, mode):
-	plaintext=pad(word)
-	encobj = AES.new(key,mode)
-	return(encobj.encrypt(plaintext))
+k_ab = scalarmult(a, b_pub)
+k_ba = scalarmult(b, a_pub)
 
-def decrypt(ciphertext,key, mode):
-	encobj = AES.new(key,mode)
-	rtn = encobj.decrypt(ciphertext)
-	return(rtn)
-
-def pad(s):
-        return s
-	extra = len(s) % 16
-	if extra > 0:
-    		s = s + (' ' * (16 - extra))
-	return s
-
-
-rnd = random.randint(1,2**128)
-
-keyA= hashlib.md5(str(rnd)).digest()
-
-rnd = random.randint(1,2**128)
-
-keyB= hashlib.md5(str(rnd)).digest()
- 
-print 'Long-term Key Alice=',binascii.hexlify(keyA)
-print 'Long-term Key Bob=',binascii.hexlify(keyB)
-
-rnd = random.randint(1,2**128)
-keySession= hashlib.md5(str(rnd)).hexdigest()
-
-ya = encrypt(keySession,keyA,AES.MODE_ECB)
-yb = encrypt(keySession,keyB,AES.MODE_ECB)
-
-print "Encrypted key sent to Alice:",binascii.hexlify(ya)
-print "Encrypted key sent to Bob:",binascii.hexlify(yb)
-
-decipherA = decrypt(ya,keyA,AES.MODE_ECB)
-decipherB = decrypt(yb,keyB,AES.MODE_ECB)
-
-print "Session key:",decipherA
-print "Session key:",decipherB
+print "Bob public: ",binascii.hexlify(b_pub)
+print "Alice public: ",binascii.hexlify(a_pub)
+print "Bob shared: ",binascii.hexlify(k_ba)
+print "Alice shared: ",binascii.hexlify(k_ab)
 
