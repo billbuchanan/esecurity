4da2de6d0dba18323e1de3d69ea08b1c5205dc91
Bill Buchanan <w.buchanan@napier.ac.uk>
2020-11-25 18:12:20 +0000

    Update g_01.py

--- unit03_hashing/src/g_01.py
+++ unit03_hashing/src/g_01.py
@@ -12,6 +12,7 @@ if (len(sys.argv)>1):
 if (len(sys.argv)>2):
 	salt=sys.argv[2]
 
-print "PBKDF2 (SHA1):"+passlib.hash.pbkdf2_sha1.encrypt(string, salt=salt)
-print "PBKDF2 (SHA256):"+passlib.hash.pbkdf2_sha256.encrypt(string, salt=salt)
+print ("PBKDF2 (SHA1):"+passlib.hash.pbkdf2_sha1.encrypt(string, salt=salt.encode()))
+print ("PBKDF2 (SHA256):"+passlib.hash.pbkdf2_sha256.encrypt(string, salt=salt.encode()))
+
 
