1538254f1a291bbc95c1b40070987ae7f1871b2e
Bill Buchanan <w.buchanan@napier.ac.uk>
2020-11-25 18:07:09 +0000

    Update e_01.py

--- unit03_hashing/src/e_01.py
+++ unit03_hashing/src/e_01.py
@@ -2,5 +2,6 @@ import passlib.hash;
 
 salt="PkWj6gM4"
 string="hello"
-print "APR1:"+passlib.hash.apr_md5_crypt.encrypt(string, salt=salt)
+print ("APR1:"+passlib.hash.apr_md5_crypt.encrypt(string, salt=salt))
+
 
